f8c966329024d690ca1f9868dc30ff4e2832eb46
Joel Knighton <joel.knighton@datastax.com>
2016-02-24 13:16:19 -0600

    Cleaned up

--- src/lwtclient/core.clj
+++ src/lwtclient/core.clj
@@ -38,8 +38,65 @@
   []
   (System/nanoTime))
 
+(defn cas
+  [session upper-bound pst op]
+  (try
+    (let [[v v'] [(rand-int upper-bound) (rand-int upper-bound)]
+          result (alia/execute session pst {:values [v' (:register op) v]})]
+      (if (-> result first ak)
+        (assoc op :type :ok :value [v v'])
+        (assoc op :type :fail :value [v v'])))
+    (catch UnavailableException e
+      (assoc op :type :fail :value :unavailable))
+    (catch ReadTimeoutException e
+      (assoc op :type :info :value :read-timed-out))
+    (catch WriteTimeoutException e
+      (assoc op :type :info :value :write-timed-out))
+    (catch NoHostAvailableException e
+      (Thread/sleep 1000)
+      (assoc op :type :fail :value :nohost))))
+
+(defn write
+  [session upper-bound pst pst-non-existent op]
+  (try
+    (let [v (rand-int upper-bound)
+          register (:register op)
+          result (alia/execute session pst {:values [v register]})]
+      (if (-> result first ak)
+        (assoc op :type :ok :value v)
+        (let [result' (alia/execute session pst-non-existent {:values [register v]})]
+          (if (-> result' first ak)
+            (assoc op :type :ok :value v)
+            (assoc op :type :fail :value v)))))
+    (catch UnavailableException e
+      (assoc op :type :fail))
+    (catch ReadTimeoutException e
+      (assoc op :type :info :value :read-timed-out))
+    (catch WriteTimeoutException e
+      (assoc op :type :info :value :write-timed-out))
+    (catch NoHostAvailableException e
+      (Thread/sleep 1000)
+      (assoc op :type :fail))))
+
+(defn read
+  [session pst op]
+  (try
+    (let [value (-> (alia/execute session pst {:values [(:register op)]
+                                               :serial-consistency :serial} )
+                    first :contents)]
+      (assoc op :type :ok :value value))
+    (catch UnavailableException e
+      (assoc op :type :fail))
+    (catch ReadTimeoutException e
+      (assoc op :type :fail))
+    (catch WriteTimeoutException e
+      (assoc op :type :fail))
+    (catch NoHostAvailableException e
+      (Thread/sleep 1000)
+      (assoc op :type :fail))))
+
 (defn -main
-  "Entry point for CALI app"
+  "Entry point for CLI app"
   [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
     (cond
@@ -54,68 +111,27 @@
             corrected-time (fn [] (+ provided-time-base (- (linear-time-nanos) relative-time-base)))
             upper-bound (:upper-bound options)
             register-count (:registers options)
-            prepared-read (alia/prepare session "SELECT * FROM REGISTERS2 WHERE ID = ?")
-            prepared-write (alia/prepare session "UPDATE REGISTERS2 SET contents=? WHERE id=? IF EXISTS")
-            prepared-cas (alia/prepare session "UPDATE REGISTERS2 SET contents=? WHERE id=? IF contents=?")]
+            prepared-read (alia/prepare session
+                                        "SELECT * FROM registers WHERE ID = ?")
+            prepared-write (alia/prepare session
+                                         "UPDATE registers SET contents=? WHERE id=? IF EXISTS")
+            prepared-write-not-exists (alia/prepare session
+                                                    "INSERT INTO registers (id, contents) VALUES (?, ?) IF NOT EXISTS")
+            prepared-cas (alia/prepare session
+                                       "UPDATE registers SET contents=? WHERE id=? IF contents=?")]
         (dotimes [n (:operation-count options)]
-          (println n)
-          (let [f (rand-nth [:cas])
+          (let [f (rand-nth [:cas :write :read])
                 register (rand-int register-count)
                 op {:type :invoke :f f
                     :time (corrected-time)
                     :register register}]
             (println op)
-            (case f
-              :cas (try
-                     (let [[v v'] [(rand-int upper-bound) (rand-int upper-bound)]
-                           result (alia/execute session prepared-cas {:values [v' register v]
-                                                                      :consistency :all})]
-                       (println [v v'])
-                       (if (-> result first ak)
-                         (println (assoc op :type :ok :value [v v'] :time (corrected-time)))
-                         (println (assoc op :type :fail :value [v v'] :time (corrected-time)))))
-                     (catch UnavailableException e
-                       (println (assoc op :type :fail :time (corrected-time))))
-                     (catch ReadTimeoutException e
-                       (println (assoc op :type :info :value :read-timed-out :time (corrected-time))))
-                     (catch WriteTimeoutException e
-                       (println (assoc op :type :info :value :write-timed-out :time (corrected-time))))
-                     (catch NoHostAvailableException e
-                       (Thread/sleep 1000)
-                       (println (assoc op :type :fail :time (corrected-time)))))
-              :write (try
-                       (let [v (rand-int upper-bound)
-                             result (alia/execute session prepared-write {:values [v register]})]
-                         (if (-> result first ak)
-                           (println (assoc op :type :ok :value v))
-                           (let [result' (alia/execute session "INSERT INTO registers2 (id, contents) VALUES (register, v) IF NOT EXISTS")]
-                             (println (-> result' first))
-                             (if (-> result' first ak)
-                               (println (assoc op :type :ok :value v :time (corrected-time)))
-                               (println (assoc op :type :fail :value v :time (corrected-time)))))))
-                       (catch UnavailableException e
-                         (println (assoc op :type :fail :time (corrected-time))))
-                       (catch ReadTimeoutException e
-                         (println (assoc op :type :info :value :read-timed-out :time (corrected-time))))
-                       (catch WriteTimeoutException e
-                         (println (assoc op :type :info :value :write-timed-out :time (corrected-time))))
-                       (catch NoHostAvailableException e
-                         (Thread/sleep 1000)
-                         (println (assoc op :type :fail :time (corrected-time)))))
-              :read (try
-                      (let [value (-> (alia/execute session prepared-read {:values [register]
-                                                                           :serial-consistency :serial} )
-                                      first :contents)]
-                        (println (assoc op :type :ok :value value :time (corrected-time))))
-                      (catch UnavailableException e
-                        (println (assoc op :type :fail :time (corrected-time))))
-                      (catch ReadTimeoutException e
-                        (println (assoc op :type :fail :time (corrected-time))))
-                      (catch WriteTimeoutException e
-                        (println (assoc op :type :fail :time (corrected-time))))
-                      (catch NoHostAvailableException e
-                        (Thread/sleep 1000)
-                        (println (assoc op :type :fail :time (corrected-time))))))))
+            (println (assoc (case f
+                              :cas (cas session upper-bound prepared-cas op)
+                              :write (write session upper-bound prepared-write
+                                            prepared-write-not-exists op)
+                              :read (read session prepared-read op))
+                            :time (corrected-time)))))
         (alia/shutdown session)
         (alia/shutdown cluster))
       (catch Exception e
