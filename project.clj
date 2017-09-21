65e51235e0a1e80a4fc66edcf6be2e2f16ae8845
Joel Knighton <joel.knighton@datastax.com>
2017-09-21 15:24:06 -0500

    Update alia. Use explicit register set. Properly handle errors. Assoc values to invoke for new format. Set consistency to serial on reads. Print ops in time-sorted order using locking. Have each thread choose a random register per op. Share process counter between threads. Share adjusted timing between threads.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lwtclient "0.2.0"
+(defproject lwtclient "0.3.0"
   :description "Performs LWT operations against a Cassandra cluster"
   :url "https://github.com/jkni/lwtclient"
   :license {:name "The Apache Software License, Version 2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.3"]
-                 [cc.qbits/alia-all "3.1.3"]
+                 [cc.qbits/alia "4.0.1"]
                  [ch.qos.logback/logback-classic "1.1.5"]]
   :main ^:skip-aot lwtclient.core
   :target-path "target/%s"
--- src/lwtclient/core.clj
+++ src/lwtclient/core.clj
@@ -10,10 +10,15 @@
   (:gen-class))
 
 (def cli-options
-  [["-r" "--register-start ID" "Starting register id"
-    :default 1
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 %) "Must be greater than 0"]]
+  [["-r" "--register-set SET" "Set of registers to operate against, as comma-separated list like 3,4,5"
+    :default [(int 1)]
+    :parse-fn (fn [arg]
+                  (as-> arg v
+                        (str/split v #",")
+                        (map #(Integer/parseInt %) v)
+                        (set v)
+                        (vec v)))
+    :validate [#(seq %) "Must provide at least one register"]]
    ["-t" "--thread-count COUNT" "Number of LWT threads to run concurrently"
     :default 1
     :parse-fn #(Integer/parseInt %)
@@ -31,7 +36,7 @@
     :validate [#(< -1 %) "Must be greater than or equal to 0"]]
    ["-p" "--print-schema" "Schema to use for cluster under load"]
    ["-H" "--hosts HOSTS" "Hosts to contact"
-    :default "localhost"
+    :default ["localhost"]
     :parse-fn #(str/split % #",")
     :validate [#(seq %) "Must provide at least one host"]]
    ["-h" "--help"]])
@@ -46,59 +51,57 @@
 (defn cas
   [session upper-bound pst op]
   (try
-    (let [[v v'] [(rand-int upper-bound) (rand-int upper-bound)]
+    (let [[v v'] (:value op)
           result (alia/execute session pst {:values [v' (:register op) v]})]
       (if (-> result first ak)
-        (assoc op :type :ok :value [v v'])
-        (assoc op :type :fail :value [v v'])))
-    (catch UnavailableException e
-      (assoc op :type :fail :value :unavailable))
-    (catch ReadTimeoutException e
-      (assoc op :type :info :value :read-timed-out))
-    (catch WriteTimeoutException e
-      (assoc op :type :info :value :write-timed-out))
-    (catch NoHostAvailableException e
-      (Thread/sleep 1000)
-      (assoc op :type :fail :value :nohost))))
+        (assoc op :type :ok)
+        (assoc op :type :fail)))
+    (catch Exception e
+      (let [driver-exception (.getCause e)]
+        (cond
+          (instance? UnavailableException driver-exception) (assoc op :type :fail :cause :unavailable)
+          (instance? ReadTimeoutException driver-exception) (assoc op :type :info :cause :read-timed-out)
+          (instance? WriteTimeoutException driver-exception) (assoc op :type :info :cause :write-timed-out)
+          (instance? NoHostAvailableException driver-exception) (do (Thread/sleep 1000) (assoc op :type :fail :cause :nohost))
+          :else (assoc op :type :error :cause :unhandled-exception :details (.toString e)))))))
 
 (defn write
   [session upper-bound pst pst-non-existent op]
   (try
-    (let [v (rand-int upper-bound)
+    (let [v (:value op)
           register (:register op)
           result (alia/execute session pst {:values [v register]})]
       (if (-> result first ak)
-        (assoc op :type :ok :value v)
+        (assoc op :type :ok)
         (let [result' (alia/execute session pst-non-existent {:values [register v]})]
           (if (-> result' first ak)
-            (assoc op :type :ok :value v)
-            (assoc op :type :fail :value v)))))
-    (catch UnavailableException e
-      (assoc op :type :fail))
-    (catch ReadTimeoutException e
-      (assoc op :type :info :value :read-timed-out))
-    (catch WriteTimeoutException e
-      (assoc op :type :info :value :write-timed-out))
-    (catch NoHostAvailableException e
-      (Thread/sleep 1000)
-      (assoc op :type :fail))))
+            (assoc op :type :ok)
+            (assoc op :type :fail)))))
+    (catch Exception e
+      (let [driver-exception (.getCause e)]
+        (cond
+          (instance? UnavailableException driver-exception) (assoc op :type :fail :cause :unavailable)
+          (instance? ReadTimeoutException driver-exception) (assoc op :type :info :cause :read-timed-out)
+          (instance? WriteTimeoutException driver-exception) (assoc op :type :info :cause :write-timed-out)
+          (instance? NoHostAvailableException driver-exception) (do (Thread/sleep 1000) (assoc op :type :fail :cause :nohost))
+          :else (assoc op :type :error :cause :unhandled-exception :details (.toString e)))))))
 
 (defn rread
   [session pst op]
   (try
     (let [value (-> (alia/execute session pst {:values [(:register op)]
-                                               :serial-consistency :serial} )
+                                               :serial-consistency :serial
+                                               :consistency :serial})
                     first :contents)]
       (assoc op :type :ok :value value))
-    (catch UnavailableException e
-      (assoc op :type :fail))
-    (catch ReadTimeoutException e
-      (assoc op :type :fail))
-    (catch WriteTimeoutException e
-      (assoc op :type :fail))
-    (catch NoHostAvailableException e
-      (Thread/sleep 1000)
-      (assoc op :type :fail))))
+    (catch Exception e
+      (let [driver-exception (.getCause e)]
+        (cond
+          (instance? UnavailableException driver-exception) (assoc op :type :fail :cause :unavailable)
+          (instance? ReadTimeoutException driver-exception) (assoc op :type :fail :cause :read-timed-out)
+          (instance? WriteTimeoutException driver-exception) (assoc op :type :fail :cause :write-timed-out)
+          (instance? NoHostAvailableException driver-exception) (do (Thread/sleep 1000) (assoc op :type :fail :cause :nohost))
+          :else (assoc op :type :error :cause :unhandled-exception :details (.toString e)))))))
 
 (defn print-schema
   "Prints schema for use"
@@ -111,15 +114,23 @@
   (locking *out*
     (apply println args)))
 
+(let [make-op-lock (Object.)]
+  (defn- make-op
+    "Build op, timestamp it, print it, and return it"
+    [op time-fn]
+    (locking make-op-lock
+      (let [timestamped-op (assoc op :time (time-fn))]
+        (println timestamped-op)
+        timestamped-op))))
+  
 (defn do-lwt-writes
   "Run LWT operations against a cluster"
-  [hosts time-base register upper-bound count]
+  [hosts time-base register-set upper-bound count process-counter]
   (try
     (let [cluster (alia/cluster {:contact-points hosts})
           session (alia/connect cluster)
           _ (alia/execute session "USE lwtclient;")
-          relative-time-base (linear-time-nanos)
-          corrected-time (fn [] (+ time-base (- (linear-time-nanos) relative-time-base)))
+          corrected-time (fn [] (+ time-base (linear-time-nanos)))
           prepared-read (alia/prepare session
                                       "SELECT * FROM registers WHERE ID = ?")
           prepared-write (alia/prepare session
@@ -127,24 +138,28 @@
           prepared-write-not-exists (alia/prepare session
                                                   "INSERT INTO registers (id, contents) VALUES (?, ?) IF NOT EXISTS")
           prepared-cas (alia/prepare session
-                                     "UPDATE registers SET contents=? WHERE id=? IF contents=?")
-          process (atom 0)]
-      (dotimes [n count]
+                                     "UPDATE registers SET contents=? WHERE id=? IF contents=?")]
+      (loop [n 0
+             my-process (swap! process-counter inc)]
         (let [f (rand-nth [:cas :write :read])
-              op {:type :invoke :f f
-                  :time (corrected-time)
-                  :process @process
-                  :register register}
-              new-op (assoc (case f
-                              :cas (cas session upper-bound prepared-cas op)
-                              :write (write session upper-bound prepared-write
-                                            prepared-write-not-exists op)
-                              :read (rread session prepared-read op))
-                            :time (corrected-time))]
-          (when (= :info (:type new-op))
-            (swap! process inc))
-          (lprintln op)
-          (lprintln new-op)))
+              value (case f
+                      :cas [(rand-int upper-bound) (rand-int upper-bound)]
+                      :write (rand-int upper-bound)
+                      :read nil)
+              op (make-op {:type :invoke :f f
+                           :lwtprocess my-process
+                           :value value
+                           :register (rand-nth register-set)}
+                          corrected-time)
+              new-op (make-op (case f
+                                :cas (cas session upper-bound prepared-cas op)
+                                :write (write session upper-bound prepared-write prepared-write-not-exists op)
+                                :read (rread session prepared-read op))
+                              corrected-time)]
+          (when (< n count)
+            (recur (inc n) (if (= :info (:type new-op))
+                               (swap! process-counter inc)
+                               my-process)))))
       (alia/shutdown session)
       (alia/shutdown cluster))
     (catch Exception e
@@ -160,9 +175,11 @@
       (:print-schema options) (print-schema)
       errors (do (println summary) (println errors) (System/exit 1))
       :else
-      (let [{:keys [hosts start-time upper-bound register-start
+      (let [{:keys [hosts start-time upper-bound register-set
                     operation-count thread-count]} options
-            count (quot operation-count thread-count)]
-        (doseq [n (range register-start (inc (+ register-start thread-count)))]
-          (future (do-lwt-writes hosts start-time (int n) upper-bound count)))
+            count (quot operation-count thread-count)
+            process-counter (atom 0)
+            modified-start-time (- start-time (linear-time-nanos))]
+        (dotimes [_ thread-count]
+          (future (do-lwt-writes hosts modified-start-time register-set upper-bound count process-counter)))
         (shutdown-agents)))))
